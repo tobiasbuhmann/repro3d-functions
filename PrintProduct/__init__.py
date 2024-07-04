7ae0788ec6816011cfe65745e119734cf25577e1
Tobias Buhmann <tobias.buhmann@ebcont.com>
2024-07-04 18:17:02 +0200

    Updated function to accept shopfiy webhook

--- PrintProduct/__init__.py
+++ PrintProduct/__init__.py
@@ -1,4 +1,7 @@
 import logging
+import hmac
+import hashlib
+import base64
 import requests
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
@@ -6,12 +9,13 @@ from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
 
 # Define variables
-TUNNEL_URL_SECRET_NAME = 'TunnelUrl'
-OCTOPRINT_API_KEY_SECRET_NAME = 'OctoprintApiKey'
 KEY_VAULT_URL = 'https://kv-repro3d.vault.azure.net'
 STORAGE_ACCOUNT_URL = 'https://repro3d.blob.core.windows.net'
 CONTAINER_NAME = 'prusa-mk4'
 BLOB_NAME = 'prusa-mk4_ring.gcode'
+TUNNEL_URL_SECRET_NAME = 'TunnelUrl'
+OCTOPRINT_API_KEY_SECRET_NAME = 'OctoprintApiKey'
+SHOPIFY_SECRET_NAME = "ShopifySecret"
 
 # Initialize Azure credentials
 credential = DefaultAzureCredential()
@@ -58,26 +62,41 @@ def upload_file_to_octoprint(tunnel_url, api_key, file_name, file_content):
         logging.error(f"Error uploading file to OctoPrint: {str(e)}")
         raise
 
+# Verify Shopify webhook
+def verify_shopify_webhook(req, shopify_secret):
+    hmac_header = req.headers.get('X-Shopify-Hmac-Sha256')
+    if not hmac_header:
+        return False
+
+    body = req.get_body()
+    hash = hmac.new(shopify_secret.encode(), body, hashlib.sha256).digest()
+    calculated_hmac = base64.b64encode(hash).decode()
+
+    return hmac_header == calculated_hmac
+
 # Main
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    # Retrieve secrets from Key Vault
+    try:
+        TUNNEL_URL = get_secret(TUNNEL_URL_SECRET_NAME)
+        OCTOPRINT_API_KEY = get_secret(OCTOPRINT_API_KEY_SECRET_NAME)
+        SHOPIFY_SECRET = get_secret(SHOPIFY_SECRET_NAME)
+    except Exception as e:
+        return func.HttpResponse(
+            f"Error retrieving secrets: {str(e)}",
+            status_code=500
+        )
+
+    # Verify the webhook signature
+    if not verify_shopify_webhook(req, SHOPIFY_SECRET):
+        return func.HttpResponse("Unauthorized", status_code=401)
 
     # Download the blob content to memory
     blob_content = download_blob_to_memory(STORAGE_ACCOUNT_URL, CONTAINER_NAME, BLOB_NAME)
     if blob_content is None:
         return func.HttpResponse("An error occurred while downloading the blob.", status_code=500)
-    
-    # Retrieve secrets from Key Vault
-    TUNNEL_URL = get_secret(TUNNEL_URL_SECRET_NAME)
-    OCTOPRINT_API_KEY = get_secret(OCTOPRINT_API_KEY_SECRET_NAME)
-
-    # Verify secrets
-    if not TUNNEL_URL or not OCTOPRINT_API_KEY:
-        logging.error("One or more secrets are not set.")
-        return func.HttpResponse(
-            "One or more secrets are not set.",
-            status_code=500
-        )
 
     # Upload the file to OctoPrint
     try:
